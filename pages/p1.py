55a09f38304cfc81fa37e030a36e615f594b6528
mrkiasu <jackster.seah@gmail.com>
2024-11-03 14:36:36 +0800

    added pages

---
+++ pages/p1.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("About Us")
+    st.write("This is the content of Page 1.")
+
+if __name__ == "__main__":
+    main()
---
+++ pages/p2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("Page 2")
+    st.write("This is the content of Page 2.")
+
+if __name__ == "__main__":
+    main()
